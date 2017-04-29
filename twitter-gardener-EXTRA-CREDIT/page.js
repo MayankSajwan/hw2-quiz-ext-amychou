585286cee1d0ac123cfa584f3a34f0d7830ada7a
achou <amychou124@gmail.com>
2017-04-28 23:36:04 -0700

    Finished project

--- twitter-gardener-EXTRA-CREDIT/page.js
+++ twitter-gardener-EXTRA-CREDIT/page.js
@@ -1,4 +1,3 @@
-// TODO(you): Add your own positive messages if you'd like!
 const POSITIVE_MESSAGES = [
   'You are worthy.',
   'You are enough.',
@@ -14,16 +13,61 @@ const POSITIVE_MESSAGES = [
   'Your life matters.'
 ];
 
+const CURSOR_IMG = chrome.runtime.getURL('images/rose-cursor.gif');
+const BACKGROUND_IMG = chrome.runtime.getURL('images/sparkle.gif');
+
 chrome.runtime.onConnect.addListener(function(port) {
   port.onMessage.addListener(onMessage);
 });
 
+function startGardening(){
+  const tweets = document.querySelectorAll(".tweet");
+  for (let tweet of tweets){
+    tweet.style.cursor = 'url(' + CURSOR_IMG + ') 4 12, auto';
+    tweet.addEventListener('mouseover', onmouseover);
+    tweet.addEventListener('mouseout', onmouseout);
+    tweet.addEventListener('click', onclick);
+  }
+}
+
+function stopGardening(){
+  const tweets = document.querySelectorAll(".tweet");
+  for (let tweet of tweets){
+    tweet.style.cursor = '';
+    tweet.removeEventListener('mouseover', onmouseover);
+    tweet.removeEventListener('mouseout', onmouseout);
+    tweet.removeEventListener('click', onclick);
+  }
+}
+
+function onclick(event){
+  const tweet = event.currentTarget;
+  event.stopPropagation();
+  const tweetText = tweet.querySelector('.tweet-text');
+  let randInd = Math.floor(Math.random() * POSITIVE_MESSAGES.length);
+  tweetText.textContent = POSITIVE_MESSAGES[randInd];
+}
+
+function onmouseover(event){
+  const tweet = event.currentTarget;
+  tweet.style.backgroundImage = 'url(' + BACKGROUND_IMG + ')';
+  tweet.style.opacity = '0.8';
+}
+
+function onmouseout(event){
+  const tweet = event.currentTarget;
+  tweet.style.backgroundImage = '';
+  tweet.style.opacity = '';
+}
+
+
 function onMessage(gardeningInProgress) {
-  // TODO(you): Implement this function for extra credit! Add helper functions
-  // as needed.
-
-  // NOTE: This extension is EXTRA CREDIT and is not required for HW2.
-
   // If `gardeningInProgress` is true, that means "Start Gardening" was clicked.
   // If `gardeningInProgress` is false, that means "Stop Gardening" was clicked.
+  if (gardeningInProgress) {
+    startGardening();
+  }
+  else{
+    stopGardening();
+  }
 }
